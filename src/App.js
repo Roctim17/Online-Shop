06662e9c51a639652f03223892cfc432f8c282c1
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-12 00:48:28 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Routes ,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     < >
-    <Header></Header>
+      <Header></Header>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/' element={<Contact/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<Contact />}></Route>
 
       </Routes>
-     <Footer></Footer>
+      <Footer></Footer>
+      <ToastContainer />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,91 @@
 import React from 'react';
 import styles from "./Header.module.scss";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FaShoppingCart, FaTimes,FaUserCircle } from "react-icons/fa";
+
+
+const logo = (
+    <div className={styles.logo}>
+        <Link to="/">
+            <h2>
+                e<span>Shop</span>.
+            </h2>
+        </Link>
+    </div>
+);
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
+const cart = (
+    <span className={styles.cart}>
+        <Link to="/cart">
+            Cart
+            <FaShoppingCart size={20} />
+            <p>0</p>
+        </Link>
+    </span>
+);
 
 const Header = () => {
+
     return (
         <header>
-        <div className={styles.header}>
-            <div className={styles.logo}>
-                <Link to='/'>
-                    <h2>
-                        Online-<span>Shop</span>. 
-                    </h2>
-                </Link>
+            <div className={styles.header}>
+                {logo}
+                <nav>
+                    <ul >
+                        <li className={styles["logo-mobile"]}>
+                            {logo}
+                            <FaTimes size={22} color="#fff" />
+                        </li>
+                        <li>
+
+                            <Link to="/admin/home">
+                                <button className="--btn --btn-primary">Admin</button>
+                            </Link>
+
+                        </li>
+                        <li>
+                            <NavLink to="/" className={activeLink}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" className={activeLink}>
+                                Contact Us
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <div className={styles["header-right"]} >
+                        <span className={styles.links}>
+                           
+                                <NavLink to="/login" className={activeLink}>
+                                    Login
+                                </NavLink>
+                        
+                           
+                                <a href="#home" style={{ color: "#ff7722" }}>
+                                    <FaUserCircle size={16} />
+                                    Hi, 
+                                </a>
+                           
+                          
+                                <NavLink to="/order-history" className={activeLink}>
+                                    My Orders
+                                </NavLink>
+                            
+                          
+                                <NavLink to="/" >
+                                    Logout
+                                </NavLink>
+                          
+                        </span>
+                        {cart}
+                    </div>
+                </nav>
+                <div className={styles["menu-icon"]}>
+                    {cart}
+
+                </div>
             </div>
-        </div>
         </header>
     );
 };
