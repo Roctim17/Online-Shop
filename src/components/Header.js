000c545bbd527e9aec9e0261efed1465016e1aca
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-17 03:42:32 +0600

    responsive mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Header.module.scss";
 import { Link, NavLink } from 'react-router-dom';
-import { FaShoppingCart, FaTimes,FaUserCircle } from "react-icons/fa";
+import { FaShoppingCart, FaTimes, FaUserCircle } from "react-icons/fa";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 
 const logo = (
@@ -25,65 +26,86 @@ const cart = (
 );
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [displayName, setdisplayName] = useState("");
+    const [scrollPage, setScrollPage] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+    const hideMenu = () => {
+        setShowMenu(false);
+    };
 
     return (
         <header>
             <div className={styles.header}>
                 {logo}
-                <nav>
-                    <ul >
-                        <li className={styles["logo-mobile"]}>
-                            {logo}
-                            <FaTimes size={22} color="#fff" />
-                        </li>
-                        <li>
+                <nav className={
+                    showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+                }>
 
-                            <Link to="/admin/home">
-                                <button className="--btn --btn-primary">Admin</button>
-                            </Link>
+                    <div className={
+                        showMenu
+                            ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                            : `${styles["nav-wrapper"]}`
+                    }
+                        onClick={hideMenu}
+                    ></div>
+                        <ul onClick={hideMenu}>
+                            <li className={styles["logo-mobile"]}>
+                                {logo}
+                                <FaTimes size={22} color="#fff" onClick={hideMenu} />
+                            </li>
+                            <li>
 
-                        </li>
-                        <li>
-                            <NavLink to="/" className={activeLink}>
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact" className={activeLink}>
-                                Contact Us
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <div className={styles["header-right"]} >
-                        <span className={styles.links}>
-                           
+                                <Link to="/admin/home">
+                                    <button className="--btn --btn-primary">Admin</button>
+                                </Link>
+
+                            </li>
+                            <li>
+                                <NavLink to="/" className={activeLink}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact" className={activeLink}>
+                                    Contact Us
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <div className={styles["header-right"]} onClick={hideMenu}>
+                            <span className={styles.links}>
+
                                 <NavLink to="/login" className={activeLink}>
                                     Login
                                 </NavLink>
-                        
-                           
+
+
                                 <a href="#home" style={{ color: "#ff7722" }}>
                                     <FaUserCircle size={16} />
-                                    Hi, 
+                                    Hi,
                                 </a>
-                           
-                          
+
+
                                 <NavLink to="/order-history" className={activeLink}>
                                     My Orders
                                 </NavLink>
-                            
-                          
-                                <NavLink to="/" >
-                                    Logout
-                                </NavLink>
-                          
-                        </span>
-                        {cart}
-                    </div>
+
+
+                                {/* <NavLink to="/" >
+                                Logout
+                            </NavLink> */}
+
+                            </span>
+                            {cart}
+                        </div>
+                    
                 </nav>
                 <div className={styles["menu-icon"]}>
                     {cart}
-
+                    <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
                 </div>
             </div>
         </header>
