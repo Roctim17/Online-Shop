92303e95e87355b88ef8f1903b3f9cf7f563ec1f
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-18 04:18:41 +0600

    done register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import styles from "../Style/auth.module.scss"
 import loginImg from '../assets/login.png'
 import { Link } from 'react-router-dom';
 import { FaGoogle } from "react-icons/fa";
+import Card from '../components/Card';
 
 const Login = () => {
     return (
@@ -11,8 +12,10 @@ const Login = () => {
             <div className={styles.img}>
                 <img src={loginImg} alt="Login" width="400" />
             </div>
+            <Card>
             <div className={styles.form}>
                 <h2>Login</h2>
+                
                 <form >
                     <input type="text" placeholder='Email' required />
                     <input type="password" placeholder='Password' required />
@@ -22,12 +25,14 @@ const Login = () => {
                     </div>
                     <p>-- or --</p>
                 </form>
+                
                 <button className='--btn --btn-danger --btn-block'><FaGoogle color='#fff'/> Login With Google</button>
                 <span className={styles.register}>
                     <p>Don't have an account?</p>
                     <Link to='/register'>Register</Link>
                 </span>
             </div>
+            </Card>
         </section>
     );
 };
