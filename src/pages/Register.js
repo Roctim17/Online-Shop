d338dc85d0ac84588868cfe35d3c3a9d7a740bca
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-18 04:32:10 +0600

    done reset password

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,31 +7,25 @@ import { Link } from 'react-router-dom';
 const Register = () => {
     return (
         <section className={`container ${styles.auth}`}>
-
-      
-        <Card>
-        <div className={styles.form}>
-            <h2>Register</h2>
-            
-            <form >
-                <input type="text" placeholder='Email' required />
-                <input type="password" placeholder='Password' required />
-                <input type="password" placeholder='Confirm Password' required />
-                <button className='--btn --btn-primary --btn-block'>Register</button>
-              
-            </form>
-            
-           
-            <span className={styles.register}>
-                <p>Already have an account?</p>
-                <Link to='/login'>Login</Link>
-            </span>
-        </div>
-        </Card>
-        <div className={styles.img}>
-            <img src={registerImg} alt="Login" width="400" />
-        </div>
-    </section>
+            <Card>
+                <div className={styles.form}>
+                    <h2>Register</h2>
+                    <form >
+                        <input type="text" placeholder='Email' required />
+                        <input type="password" placeholder='Password' required />
+                        <input type="password" placeholder='Confirm Password' required />
+                        <button className='--btn --btn-primary --btn-block'>Register</button>
+                    </form>
+                    <span className={styles.register}>
+                        <p>Already have an account?</p>
+                        <Link to='/login'>Login</Link>
+                    </span>
+                </div>
+            </Card>
+            <div className={styles.img}>
+                <img src={registerImg} alt="Login" width="400" />
+            </div>
+        </section>
     );
 };
 
