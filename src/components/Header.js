e89fe5b779db3dbaa5b4ed008e4088983ce838c3
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-20 01:23:16 +0600

    logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import styles from "./Header.module.scss";
-import { Link, NavLink } from 'react-router-dom';
+import { Link,  NavLink, useNavigate } from 'react-router-dom';
 import { FaShoppingCart, FaTimes, } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
+import { toast } from 'react-toastify';
 
 
 const logo = (
@@ -25,10 +28,12 @@ const cart = (
     </span>
 );
 
+
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
     // const [displayName, setdisplayName] = useState("");
     // const [scrollPage, setScrollPage] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
@@ -36,6 +41,17 @@ const Header = () => {
     const hideMenu = () => {
         setShowMenu(false);
     };
+
+    const logoutUser = () => {
+        signOut(auth)
+          .then(() => {
+            toast.success("Logout successfully.");
+            navigate("/");
+          })
+          .catch((error) => {
+            toast.error(error.message);
+          });
+      };
 
     return (
         <header>
@@ -52,59 +68,59 @@ const Header = () => {
                     }
                         onClick={hideMenu}
                     ></div>
-                        <ul onClick={hideMenu}>
-                            <li className={styles["logo-mobile"]}>
-                                {logo}
-                                <FaTimes size={22} color="#fff" onClick={hideMenu} />
-                            </li>
-                            <li>
+                    <ul onClick={hideMenu}>
+                        <li className={styles["logo-mobile"]}>
+                            {logo}
+                            <FaTimes size={22} color="#fff" onClick={hideMenu} />
+                        </li>
+                        <li>
 
-                                <Link to="/admin/home">
-                                    <button className="--btn --btn-primary">Admin</button>
-                                </Link>
+                            <Link to="/admin/home">
+                                <button className="--btn --btn-primary">Admin</button>
+                            </Link>
 
-                            </li>
-                            <li>
-                                <NavLink to="/" className={activeLink}>
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/contact" className={activeLink}>
-                                    Contact Us
-                                </NavLink>
-                            </li>
-                        </ul>
-                        <div className={styles["header-right"]} onClick={hideMenu}>
-                            <span className={styles.links}>
+                        </li>
+                        <li>
+                            <NavLink to="/" className={activeLink}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" className={activeLink}>
+                                Contact Us
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <div className={styles["header-right"]} onClick={hideMenu}>
+                        <span className={styles.links}>
 
-                                <NavLink to="/login" className={activeLink}>
-                                    Login
-                                </NavLink>
-                                <NavLink to="/register" className={activeLink}>
-                                    Register
-                                </NavLink>
+                            <NavLink to="/login" className={activeLink}>
+                                Login
+                            </NavLink>
+                            <NavLink to="/register" className={activeLink}>
+                                Register
+                            </NavLink>
 
 
-                                {/* <a href="#home" style={{ color: "#ff7722" }}>
+                            {/* <a href="#home" style={{ color: "#ff7722" }}>
                                     <FaUserCircle size={16} />
                                     Hi,
                                 </a> */}
 
 
-                                <NavLink to="/order-history" className={activeLink}>
-                                    My Orders
-                                </NavLink>
+                            <NavLink to="/order-history" className={activeLink}>
+                                My Orders
+                            </NavLink>
 
 
-                                {/* <NavLink to="/" >
+                            <NavLink to="/" onClick={logoutUser}>
                                 Logout
-                            </NavLink> */}
+                            </NavLink>
 
-                            </span>
-                            {cart}
-                        </div>
-                    
+                        </span>
+                        {cart}
+                    </div>
+
                 </nav>
                 <div className={styles["menu-icon"]}>
                     {cart}
