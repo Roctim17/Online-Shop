2c6662e1bf3671c31aa60929b774dd2a88cc51dd
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-21 05:14:12 +0600

    done

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai"
 import "../Style/Slider.scss";
 import { sliderData } from "../assets/slider-data";
@@ -7,17 +7,37 @@ const Slider = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const slideLength = sliderData.length;
 
-const nextSlide =()=>{
-    setCurrentSlide(currentSlide === slideLength-1 ? 0 :currentSlide + 1);
-};
-const prevSlide =()=>{
-    setCurrentSlide(currentSlide === 0 ? slideLength-1 :currentSlide - 1);
-};
+    const autoScroll = true
+    let slideInterval;
+    let intervalTime = 5000;
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    };
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+    };
+
+    useEffect(() => {
+        setCurrentSlide(0)
+    }, [])
+    // const auto =()=> {
+    //     slideInterval = setInterval(nextSlide, intervalTime)
+    // }
+    useEffect(() => {
+        if (autoScroll) {
+            const auto =()=> {
+                slideInterval = setInterval(nextSlide, intervalTime)
+            };
+            auto()
+        }
+        return ()=> clearInterval(slideInterval)
+    }, [currentSlide, slideInterval, autoScroll])
 
     return (
         <div className='slider'>
             <AiOutlineArrowLeft className='arrow prev' onClick={prevSlide} />
-            <AiOutlineArrowRight className='arrow next' onClick={nextSlide}/>
+            <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
             {sliderData.map((slide, index) => {
                 const { image, heading, desc } = slide
                 return (
